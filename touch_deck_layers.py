cd126394e58f9006d84c57d48ca78e95d6c3e4a7
foamyguy <foamyguy@gmail.com>
2021-03-12 20:41:49 -0600

    new icons. update layer config

--- touch_deck_layers.py
+++ touch_deck_layers.py
@@ -8,33 +8,33 @@ MEDIA = 1
 KEY = 2
 
 touch_deck_config = {
-    "layers": [
+    "layers":[
         {
             "name": "Youtube Controls",
             "shortcuts": [
                 {
                     "label": "Play",
-                    "icon": "touch_deck_icons/test32_icon.bmp",
+                    "icon": "touch_deck_icons/Play_48x48.bmp",
                     "actions": (KEY, [Keycode.K])
                 },
                 {
                     "label": "Pause",
-                    "icon": "touch_deck_icons/test32_icon.bmp",
+                    "icon": "touch_deck_icons/Pause_48x48.bmp",
                     "actions": (KEY, [Keycode.K])
                 },
                 {
                     "label": "FastForward",
-                    "icon": "touch_deck_icons/test32_icon.bmp",
+                    "icon": "touch_deck_icons/test48_icon.bmp",
                     "actions": (KEY, [Keycode.RIGHT_ARROW])
                 },
                 {
                     "label": "Rewind",
-                    "icon": "touch_deck_icons/test32_icon.bmp",
+                    "icon": "touch_deck_icons/Previous_48x48.bmp",
                     "actions": (KEY, [Keycode.LEFT_ARROW])
                 },
                 {
                     "label": "Next",
-                    "icon": "touch_deck_icons/test32_icon.bmp",
+                    "icon": "touch_deck_icons/Next_48x48.bmp",
                     "actions": (KEY, [Keycode.RIGHT_SHIFT, Keycode.N])
                 }
             ]
